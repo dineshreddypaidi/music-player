c88e87b98e8a0e4cb751206db4a2509ed56144c4
manasa <akuthotamanasa19@gmail.com>
2024-06-19 21:44:10 +0530

    changes:

--- songs.py
+++ songs.py
@@ -1,8 +1,10 @@
 import os
 import fnmatch
+import os
 
 class Songs:
     def FindSong(patterns,paths):
+        print("searching for songs in the OS")
         songpath = []  
         namees = []    
         for pattern in patterns:
@@ -13,12 +15,21 @@ class Songs:
                         if fnmatch.fnmatch(name, pattern) and os.path.getsize(file_path) > 1024 * 1024:
                             songpath.append(file_path)
                             namees.append(name)
+        print("searching completed..")
         return songpath,namees
-        
-    songextensions = ["*.mp3","*.m4a"]
-    searchpaths = ["C:/Users","D:/","E:/"]
+    
+    def is_empty():
+        return True if len(Songs.songpath) == 0 else False
+    
+    songextensions = ["*.mp3",]
+    
+    osname = os.name
+    if osname == 'nt':
+        searchpaths = ["C:/Users","D:/","E:/"]     
+    if osname == 'posix':
+        searchpaths = ["/","/usr"]   
     
     songpath, songname  = FindSong(songextensions,searchpaths)
-
+    
 if __name__ == "__main__" :
-       Songs()    
+       Songs()
